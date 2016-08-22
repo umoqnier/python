0f47746c7d9a85a5c5d69dcc98ecd47b35e2015a
Diego Alberto Barriga Martínez <umoqnier@gmail.com>
2016-08-22 12:32:12 -0500

    Función Agregada
    Se agrego la función fibo_3 que utiliza generadores para mostrar la
    la seríe.

--- fibonacci.py
+++ fibonacci.py
@@ -5,4 +5,12 @@ def fibo_1(max):
         print(a)
         a, b = b, a+b
 
+def fibo_3(max):
+    a, b = 0, 1
+    while a < max:
+        yield a
+        a, b = b, a+b
+
 fibo_1(10)
+for n in fibo_3(10):
+    print(n)
