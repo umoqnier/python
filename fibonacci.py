1ef8a1a979e9790f49716592a9dc50e4e4557d1b
Luis <jluiserazod@yahoo.com.mx>
2016-08-22 12:34:06 +0200

    Agregegada la funcion fibo_2
    Esta funcion regresa una lista con la serie

--- fibonacci.py
+++ fibonacci.py
@@ -5,4 +5,15 @@ def fibo_1(max):
         print(a)
         a, b = b, a+b
 
+def fibo_2(max):
+	serie = []
+	a, b = 0, 1
+	
+	while a < max:
+		serie.append(a)
+		a, b = b, a+b
+	return serie
+	
 fibo_1(10)
+print(fibo_2(10))
+
